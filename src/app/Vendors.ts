5c14bc4fc9760b33b5fade907091eafe7b1398d3
heep-jay <jubrilmuritala69@gmail.com>
2022-11-15 02:47:47 -0500

    responsiveness

--- src/app/Vendors.ts
+++ src/app/Vendors.ts
@@ -1,11 +1,12 @@
 export interface Vendor {
 
   id?: number;
-  image: string;
+  image?: string;
   name: string;
   b_range: number;
   desc: string;
   rating: string;
   reviews: number;
+  services?: any
 
 }
