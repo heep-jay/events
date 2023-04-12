fe9cdeeb359e5d4c280a2b7adfb9c062cec607ef
heep-jay <jubrilmuritala69@gmail.com>
2023-04-12 07:36:04 -0500

    added templates styling

--- src/app/Component/wedinvite/wedinvite.component.ts
+++ src/app/Component/wedinvite/wedinvite.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wedinvite.component.css']
 })
 export class WedinviteComponent implements OnInit {
-
+husband_name: string = "Matt"
+wife_name: string = "chloe"
+date:string = "10"
+year:string = "2022"
+weekday: string = "Sunday"
+month:string = "July"
+time:string = "AT 5PM"
   constructor() { }
 
   ngOnInit(): void {
